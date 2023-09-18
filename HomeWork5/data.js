9f29b8179799d08f462ce8c6aa48e07c2e459386
LeaClover <plesen_14@mail.ru>
2023-09-19 00:09:40 +0300

    add new project hw5

---
+++ HomeWork5/data.js
@@ -0,0 +1,73 @@
+﻿export const jokes = `[
+    {
+      "type": "programming",
+      "setup": "Why did the programmer quit his job?",
+      "punchline": "Because he didn't get arrays.",
+      "id": 18,
+      "img": "https://cs14.pikabu.ru/post_img/2023/02/13/8/1676295806139337963.jpg"
+    },
+    {
+      "type": "general",
+      "setup": "What did the judge say to the dentist?",
+      "punchline": "Do you swear to pull the tooth, the whole tooth and nothing but the tooth?",
+      "id": 176,
+      "img": "https://cs13.pikabu.ru/post_img/big/2023/02/13/8/1676295806122712757.png"
+    },
+    {
+      "type": "general",
+      "setup": "What do you call a boomerang that won't come back?",
+      "punchline": "A stick.",
+      "id": 195,
+      "img": "https://bipbap.ru/wp-content/uploads/2022/11/1652235714_41-kartinkin-net-p-prikolnie-kartinki-dlya-stima-44.jpg"
+    },
+    {
+      "type": "general",
+      "setup": "Why do mathematicians hate the U.S.?",
+      "punchline": "Because it's indivisible.",
+      "id": 354,
+      "img": "https://shapka-youtube.ru/wp-content/uploads/2021/02/avatarka-dlya-skaypa-dlya-parney.jpg"
+    },
+    {
+      "type": "general",
+      "setup": "What do I look like?",
+      "punchline": "A JOKE MACHINE!?",
+      "id": 190,
+      "img": "https://cs14.pikabu.ru/post_img/big/2023/02/13/8/1676295972133291028.png"
+    },
+    {
+      "type": "programming",
+      "setup": "Lady: How do I spread love in this cruel world?",
+      "punchline": "Random Dude: [...💘]",
+      "id": 59,
+      "img": "https://econet.ru/uploads/pictures/456175/content_photo_1.jpg"
+    },
+    {
+      "type": "general",
+      "setup": "What is a centipedes's favorite Beatle song?",
+      "punchline": " I want to hold your hand, hand, hand, hand...",
+      "id": 244,
+      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3vzd29nNdpmDdkZlDDUKkqptMrubOAJeSAw&usqp=CAU"
+    },
+    {
+      "type": "general",
+      "setup": "How do you make a hankie dance?",
+      "punchline": "Put a little boogie in it.",
+      "id": 128,
+      "img": "https://www.youloveit.ru/uploads/posts/2020-04/1586360148_youloveit_ru_bill_gravity_falls_na_avu11.jpg"
+    },
+    {
+      "type": "general",
+      "setup": "Why did the tree go to the dentist?",
+      "punchline": "It needed a root canal.",
+      "id": 341,
+      "img": "https://sovietgames.su/wp-content/uploads/2016/07/%D0%90%D0%B2%D0%B0%D1%82%D0%B0%D1%80%D0%BA%D0%B0.jpg"
+    },
+    {
+      "type": "programming",
+      "setup": "Hey, wanna hear a joke?",
+      "punchline": "Parsing HTML with regex.",
+      "id": 403,
+      "img": "https://koshka.top/uploads/posts/2021-12/thumbs/1638599370_2-koshka-top-p-kotiki-na-avatarku-2.jpg"
+    }
+  ]`
+
---
+++ HomeWork5/script.js
@@ -0,0 +1,81 @@
+﻿// Задание 1
+// 1. Поиск в интернете (бесплатные api примеры).
+// 2. Найти любые данные, на произвольную тему.
+// 3. Создать файл data.js.
+// 4. Создать переменную которая будет хранить данные из публичных api.
+
+// Задание 2
+// 1. Создать файл index.html.
+// 2. Подключить data.js.
+// 3. Сформировать контент из данных (картинка загловок и параграф).
+// 4. Добавить данный контент в вёрстку.
+// 5. * Добавить стили при необходимости (по желанию).
+
+import { jokes } from "./data.js";
+
+const data = JSON.parse(jokes);
+
+const content = document.querySelector('.content');
+content.style.display = 'flex';
+content.style.gap = '10px';
+content.style.flexWrap = 'wrap';
+content.style.justifyContent = 'center';
+content.style.background = '#f5f5f5';
+// content.style.margin = '0 auto';
+
+data.forEach(({ id, type, setup, punchline, img }) => {
+    const div = document.createElement('div');
+    div.classList.add('block');
+    div.style.display = 'flex';
+    div.style.justifyContent = 'center';
+    div.style.flexDirection = 'column';
+    div.style.flexWrap = 'wrap';
+    div.style.marginLeft = '5px';
+    div.style.width = '40%';
+    div.style.gap = '1px';
+    div.style.border = '5px solid orange';
+    div.style.background = '#ccc';
+    div.style.marginTop = '0px';
+    content.appendChild(div);
+
+    const imgimg = document.createElement('img');
+    imgimg.classList.add('img-block');
+    imgimg.src = img;
+    imgimg.style.width = '200px';
+    imgimg.style.marginTop = '10px';
+    imgimg.style.alignSelf = 'center';
+    imgimg.style.border = '3px solid black';
+    div.appendChild(imgimg);
+
+    const h1 = document.createElement('h1');
+    h1.classList.add('block__header');
+    h1.textContent = `type of joke: ${type}`;
+    h1.style.background = 'orange';
+    h1.style.color = 'blue';
+    h1.style.padding = '2px 15px 5px';
+    h1.style.width = '50%';
+    h1.style.border = '5px solid black';
+    h1.style.borderLeft = 'none';
+    div.appendChild(h1);
+
+    const textID = document.createElement('p');
+    textID.classList.add('block-text');
+    textID.textContent = `id: ${id}`;
+    textID.style.color = 'grey';
+    textID.style.paddingLeft = '20px';
+    div.appendChild(textID);
+
+    const setupBlock = document.createElement('h4');
+    setupBlock.classList.add('block__setup');
+    setupBlock.textContent = `- ${setup}`;
+    setupBlock.style.padding = '0px 20px';
+    setupBlock.style.height = '20px';
+    div.appendChild(setupBlock);
+
+    const punchlineBlock = document.createElement('h4');
+    punchlineBlock.classList.add('block__punchline');
+    punchlineBlock.textContent = `- ${punchline}`;
+    punchlineBlock.style.padding = '0px 20px';
+    punchlineBlock.style.height = '50px';
+    div.appendChild(punchlineBlock);
+});
